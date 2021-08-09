2df7631c769df51feb34983339c2e67d813b4115
Rapidy <artemchik03211@gmail.com>
2021-08-10 01:55:41 +0400

    2

---
+++ pizza/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export default function Button(props) {
+  return (
+    <button
+      className={classNames('button', {
+        'button--outline': props.outline,
+      })}>
+      {props.children}
+    </button>
+  );
+}
