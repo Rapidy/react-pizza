941e351f9ce387a786f8ec2c6be6c38168c31ae3
Rapidy <artemchik03211@gmail.com>
2021-09-17 00:47:30 +0500

    Update pizzas.js

--- pizza/src/redux/actions/pizzas.js
+++ pizza/src/redux/actions/pizzas.js
@@ -10,7 +10,7 @@ export const fetchPizzas = (filter) => (dispatch) => {
 
   axios
     .get(
-      `http://localhost:3001/pizzas?${
+      `https://6143998f4118f80919f8e9b3--hardcore-shaw-12916d.netlify.app/pizzas?${
         filter.category !== null ? `category=${filter.category}` : ''
       }&_sort=${filter.sortBy.type}&_order=${filter.sortBy.order}`,
     )
